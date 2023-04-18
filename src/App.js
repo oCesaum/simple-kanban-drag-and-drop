f0c277f2f3afbd7256d0d58b15375383d5dfdaca
César Augusto <97255173+oCesaum@users.noreply.github.com>
2023-04-18 17:14:19 -0300

    removing Instagram link

--- src/App.js
+++ src/App.js
@@ -73,35 +73,69 @@ export default function App() {
 
   function handleDrop(e, dropzone) {
     e.preventDefault();
-
+  
     const sourceBoardIndex = boards.findIndex((board) =>
       board.cards.includes(cardBeingDragged)
     );
     const sourceBoard = boards[sourceBoardIndex];
-    const targetBoardIndex = boards.findIndex(
-      (board) => board.name === dropzone
-    );
+    const targetBoardIndex = boards.findIndex((board) => board.name === dropzone);
     const targetBoard = boards[targetBoardIndex];
-
+  
     // Removendo o cartão do quadro de origem
     sourceBoard.cards = sourceBoard.cards.filter(
       (card) => card !== cardBeingDragged
     );
-
-    // Adicionando o cartão ao quadro de destino
-    targetBoard.cards.push(cardBeingDragged);
-
+  
+    // Obter o elemento sob o mouse
+    const elementUnderMouse = document.elementFromPoint(e.clientX, e.clientY);
+  
+    // Verificar se o elemento é um cartão
+    const isCardElement =
+      elementUnderMouse &&
+      elementUnderMouse.classList.contains("card");
+  
+    let dropIndex = targetBoard.cards.length;
+  
+    if (isCardElement) {
+      // Encontrar o índice do cartão sob o mouse
+      const cardElementUnderMouse = elementUnderMouse;
+      const cardIdUnderMouse = parseInt(cardElementUnderMouse.dataset.cardId);
+      const cardIndexUnderMouse = targetBoard.cards.findIndex(
+        (card) => card.id === cardIdUnderMouse
+      );
+  
+      // Determinar se o cartão deve ser inserido antes ou depois do cartão sob o mouse
+      const cardRect = cardElementUnderMouse.getBoundingClientRect();
+      const mouseYRelativeToCard = e.clientY - cardRect.top;
+  
+      if (mouseYRelativeToCard < cardRect.height / 2) {
+        dropIndex = cardIndexUnderMouse;
+      } else {
+        dropIndex = cardIndexUnderMouse + 1;
+      }
+    }
+  
+    // Inserir o cartão na posição correta no quadro de destino
+    targetBoard.cards.splice(dropIndex, 0, cardBeingDragged);
+  
     // Atualizando o estado dos quadros
     const updatedBoards = [...boards];
     updatedBoards[sourceBoardIndex] = sourceBoard;
     updatedBoards[targetBoardIndex] = targetBoard;
     setBoards(updatedBoards);
-
+  
     // Limpando o estado de arrastar e soltar
     setCardBeingDragged(null);
     setDraggingOver(null);
     dragend();
-  }
+  }  
+
+  function getMousePositionInElement(e, element) {
+    const elementRect = element.getBoundingClientRect();
+    const x = e.clientX - elementRect.left;
+    const y = e.clientY - elementRect.top;
+    return { x, y };
+  }  
 
   function closeModal() {
     setNewBoardValue("");
@@ -297,6 +331,7 @@ export default function App() {
                             className={`card bg-[#1A1A1C] mb-7 rounded-md font-semibold text-lg w-64 p-4 shadow-custom  ${
                               cardBeingDragged === card ? "opacity-50" : ""
                             }`}
+                            data-card-id={card.id}
                             draggable="true"
                             onDragStart={() => dragstart(card)}
                             onDragEnd={() => dragend()}
@@ -650,33 +685,6 @@ export default function App() {
                 <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
               </svg>
               Linkedin
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.instagram.com/_cesaum/"
-              target={"_blank"}
-              rel="noreferrer"
-              className="flex items-center gap-2 hover:text-transparent bg-clip-text bg-gradient-to-r from-zinc-200 to-zinc-600 transition-colors duration-200 rounded-md focus-visible:outline focus-visible:outline-[#FD951FCC] group"
-            >
-              <svg
-                className="w-7 h-7 fill-white group-hover:fill-[url(#gradiente)]"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
-              >
-                <linearGradient
-                  id="gradiente"
-                  x1="0%"
-                  y1="0%"
-                  x2="100%"
-                  y2="0%"
-                >
-                  <stop offset="0%" stopColor="#E4E4E7" />
-                  <stop offset="100%" stopColor="#71717A" />
-                </linearGradient>
-                <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
-              </svg>
-              Instagram
             </a>
           </li>
         </ul>
